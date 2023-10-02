8321c7feae7f8b2e00fdb4d6ce76c6df8c82d495
bruno <brunovs.eneas@gmail.com>
2023-10-02 10:25:31 -0300

    iniciar

---
+++ ls_2023.2/Exercicio-Flags/src/pages/exercises/web-script-country-flags/_codes/js/code/js/main.js
@@ -0,0 +1,13 @@
+import flags from './model/flags.js';
+
+const main = document.querySelector('main');
+
+for (const flag of flags) {
+    main.insertAdjacentHTML('beforeend', createFlagView(flags));
+}
+
+function createFlagView(flag) {
+    return `<div class="flag col-2 my-2 text-center">
+    <img src="${flag.image}" alt="${flag.name}">
+    </div>`;
+}
